ed176f0c6f36a3e403e88dbb8e61ca4532fce99b
Manasvi <mg.manasvi@gmail.com>
2025-02-05 12:45:47 -0500

    add the logger code

---
+++ src/logger.py
@@ -0,0 +1,20 @@
+import logging
+
+class Logger:
+    def __init__(self, log_file='chat.log'):
+        logging.basicConfig(
+            filename=log_file,
+            level=logging.INFO,
+            format='%(asctime)s - %(levelname)s - %(message)s'
+        )
+        self.logger = logging.getLogger()
+    
+    def log(self, message, level='info'):
+        if level == 'info':
+            self.logger.info(message)
+        elif level == 'error':
+            self.logger.error(message)
+        elif level == 'warning':
+            self.logger.warning(message)
+        elif level == 'debug':
+            self.logger.debug(message)
